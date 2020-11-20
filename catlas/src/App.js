9f7da4b7e12fba7c19bc7bbb9cff0729085b8966
Hatchling <hatchling3713@gmail.com>
2020-11-20 21:57:26 +0900

    Worked on Links page, added static images

--- catlas/src/App.js
+++ catlas/src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import {
   Main,
@@ -12,56 +11,31 @@ import {
   Links,
   Scenes,
   Talks
-} from "./Pages";
+} from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Main</Link>
-        </li>
-        <li>
-          <Link to="/clubs">Clubs</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/talks">Talks</Link>
-        </li>
-        <li>
-          <Link to="/links">Links</Link>
-        </li>
-        <li>
-          <a href="https://github.com/GNU-CS/catlas-frontend/issues">Admin</a>
-        </li>
-        <li>
-          <Link to="/scenes">Scenes</Link>
-        </li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route exact path="/">
-        <Main />
-      </Route>
-      <Route path="/clubs">
-        <Clubs />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/talks">
-        <Talks />
-      </Route>
-      <Route path="/links">
-        <Links />
-      </Route>
-      <Route path="/scenes">
-        <Scenes />
-      </Route>
-    </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/clubs">
+          <Clubs />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/talks">
+          <Talks />
+        </Route>
+        <Route path="/links">
+          <Links />
+        </Route>
+        <Route path="/scenes">
+          <Scenes />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }

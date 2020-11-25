8ae367e974ddabd2068785b5cc1fed4640ee3709
Hatchling <hatchling3713@gmail.com>
2020-11-25 23:55:24 +0900

    Menu 컴포넌트에 끼워넣는 방식, 이후 Modal 등을 통해 Input을 받아 백엔드로 보낼 수 있다.

--- catlas/src/component/theme.jsx
+++ catlas/src/component/theme.jsx
@@ -5,6 +5,11 @@ import { Link } from "react-router-dom";
 import bgimg from "../assets/img/catlas-logo.png";
 
 function Theme() {
+    const authMenu = (isLoggedIn) => {
+        if (isLoggedIn) return '로그아웃'
+        else return '로그인'
+    }
+
     return (
         <Grid container columns={1} verticalAlign='middle' padded='vertically'>
             <Grid.Column>
@@ -13,13 +18,14 @@ function Theme() {
                 </Container>
             </Grid.Column>
             <Grid.Column>
-                <Menu pointing widths={6} stackable>
+                <Menu pointing widths={7} stackable>
                     <Menu.Item as={Link} to='clubs' name='clubs'>동아리</Menu.Item>
                     <Menu.Item as={Link} to='about' name='about'>집부</Menu.Item>
                     <Menu.Item as={Link} to='talks' name='talks'>자유게시판</Menu.Item>
                     <Menu.Item as={Link} to='links' name='links'>바로가기</Menu.Item>
                     <Menu.Item href='https://github.com/GNU-CS/catlas-frontend/issues' name='admin'>관리자에게</Menu.Item>
                     <Menu.Item as={Link} to='scenes' name='scenes'>사진첩</Menu.Item>
+                    <Menu.Item name='account'>{authMenu(false)}</Menu.Item>
                 </Menu>
             </Grid.Column>
         </Grid>
